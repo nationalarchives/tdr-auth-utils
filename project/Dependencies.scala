cb4e1325c2a3aedf2b4cd430b3c64f96de1c6de6
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-03-29 10:40:48 +0000

    Update scala-logging to 3.9.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.0-M2"
   lazy val keycloakAdapterCore  = "org.keycloak" % "keycloak-adapter-core" % keycloakVersion
   lazy val keycloakCore = "org.keycloak" % "keycloak-core" % keycloakVersion
-  lazy val logger = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
+  lazy val logger = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.4"
   lazy val httpComponents = "org.apache.httpcomponents" % "httpclient" % "4.5.11"
   lazy val jbossLogging = "org.jboss.logging" % "jboss-logging" % "3.4.1.Final"
   lazy val keycloakMock = "com.tngtech.keycloakmock" % "mock" % "0.2.0"
