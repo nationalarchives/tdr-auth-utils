2228802ef4fd02b0100b49c6ad3db098b150e098
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-07-10 08:24:26 +0000

    Update client3:circe, client3:core to 3.8.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   private lazy val circeVersion = "0.14.5"
   private lazy val keycloakVersion = "21.1.2"
-  private lazy val softWareMillVersion = "3.8.15"
+  private lazy val softWareMillVersion = "3.8.16"
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.16"
   lazy val keycloakAdapterCore  = "org.keycloak" % "keycloak-adapter-core" % keycloakVersion
