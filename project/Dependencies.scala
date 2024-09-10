0cf9651eae4cd866a53e75a127de7406401be269
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-09-10 08:22:58 +0000

    Update keycloak-core to 25.0.5 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   private lazy val circeVersion = "0.14.10"
   private lazy val softWareMillVersion = "3.9.7"
-  private lazy val keycloakVersion = "24.0.5"
+  private lazy val keycloakVersion = "25.0.5"
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
   lazy val keycloakAdapterCore  = "org.keycloak" % "keycloak-adapter-core" % keycloakVersion
